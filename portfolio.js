fe8a0baf434f34ca1774b1b0549f7d5040022651
ouissal <ouissal.attaaricha@gmail.com>
2026-03-01 04:17:58 +0000

    Fixed EmailJS contact form and added hover animations

--- portfolio.js
+++ portfolio.js
@@ -6,4 +6,24 @@ const menuToggle = document.getElementById("menu-toggle");
   });
 
 
- 
+ const btn = document.getElementById('send');
+  const contactForm = document.getElementById('contact form');
+
+  contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.innerText = 'Sending...';
+
+    const serviceID = 'service_pb2376h'; 
+    const templateID = 'template_471cywq'; 
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.innerText = 'Send Email';
+        alert('Message sent successfully!');
+        contactForm.reset();
+      }, (err) => {
+        btn.innerText = 'Send Email';
+        alert('Failed to send: ' + JSON.stringify(err));
+      });
+  });
